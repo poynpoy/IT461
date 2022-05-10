f7f19db5e8f57e46709266642caaf474564d9b5d
John Paul Eguia <Johnpauleguia_1999@yahoo.com>
2022-05-11 04:37:29 +0800

    Exer3

--- server/src/v1/cat/model.py
+++ server/src/v1/cat/model.py
@@ -2,6 +2,8 @@ from db import Db
 
 class CatModel():
     def sanitize(self, cats):
+        if not isinstance(cats, (list, tuple)):
+            cats = [cats]
         clean_cats = []
         for cat in cats:
             if not isinstance(cat, dict):
@@ -19,22 +21,42 @@ class CatModel():
             return False
         queries = []
         for cat in clean_cats:
-            sql = "INSERT INTO cats(name) VALUES (%s)"
-            queries.append({'sql':sql, 'bind': cat['name']})
+            sql = "INSERT INTO cats(name) VALUES(%s)"
+            queries.append({"sql": sql, "bind": cat['name']})
         db = Db.get_instance()
         result = db.transactional(queries)
         return cats
 
-    def read(self, filters = None):
+    def read(self, filters=None, count_only=False):
         db = Db.get_instance()
+        fields = ['*']
+        offset = 0
+        limit = 5
         if filters is not None:
+            if 'fields' in filters:
+                tmp_fields = []
+                for field in filters['fields']:
+                    if field in ['id', 'name']:
+                        tmp_fields.append(field)
+                if len(tmp_fields) > 0:
+                    fields = tmp_fields
             if 'id' in filters:
-                sql = "SELECT * FROM cats WHERE id = %s"
+                sql = "SELECT " + ','.join(fields) + " FROM cats WHERE id = %s"
                 cat = db.fetchone(sql, filters['id'])
                 return cat
-        sql = "SELECT * FROM cats ORDER BY name"
-        cats = db.fetchall(sql)
-        return cats
+            if 'offset' in filters:
+                offset = int(filters['offset'])
+            if 'limit' in filters:
+                limit = int(filters['limit'])
+        cols = 'COUNT(*) AS total' if count_only else ','.join(fields)
+        sql = "SELECT " + cols + " FROM cats"
+        if not count_only:
+            sql += " ORDER BY name LIMIT " + str(offset) + ", " + str(limit)
+        if count_only:
+            row = db.fetchone(sql)
+            return row['total'] if row else 0
+        else:
+            return db.fetchall(sql)
 
     def update(self, cats):
         if not isinstance(cats, (list, tuple)):
@@ -45,7 +67,7 @@ class CatModel():
         queries = []
         for cat in clean_cats:
             sql = "UPDATE cats SET name = %s WHERE id = %s"
-            queries.append({'sql':sql, 'bind': (cat['name'], cat['id'])})
+            queries.append({"sql": sql, "bind": (cat['name'], cat['id'])})
         db = Db.get_instance()
         db.transactional(queries)
         return cats
@@ -59,7 +81,7 @@ class CatModel():
         for cat in cats:
             placeholder.append('%s')
         sql = "DELETE FROM cats WHERE id IN (" + ", ".join(placeholder) + ")"
-        queries.append({'sql': sql, 'bind': cats})
+        queries.append({"sql": sql, "bind": cats})
         db = Db.get_instance()
         counter = db.transactional(queries)
         return counter
--- server/src/v1/dog/model.py
+++ server/src/v1/dog/model.py
@@ -1,56 +1,87 @@
-from flask import request, make_response, jsonify
-from v1.dog.model import DogModel
+from db import Db
 
-class DogController():
-    _instance = None
-
-    def __init__(self) -> None:
-        self._instance = DogModel()
-
-    def post(self):
-        print(request.json)
-        resp = self._instance.create(request.json)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-
-    def check(self, dog_id):
-        dog = self._instance.read({"id": dog_id})
-        if dog is None:
-            return make_response(jsonify({"error": "Dog id not found."}), 404)
-        return dog
-
-    def get(self, dog_id=None):
-        if dog_id is not None:
-            dog = self.check(dog_id)
+class DogModel():
+    def sanitize(self, dogs):
+        if not isinstance(dogs, (list, tuple)):
+            dogs = [dogs]
+        clean_dogs = []
+        for dog in dogs:
             if not isinstance(dog, dict):
+                continue
+            if not ('id' in dog and 'name' in dog):
+                continue
+            clean_dogs.append(dog)
+        return clean_dogs
 
-                return dog
-            return jsonify(dog)
-        dogs = self._instance.read()
-        return jsonify(dogs)
+    def create(self, dogs):
+        if not isinstance(dogs, (list, tuple)):
+            dogs = [dogs]
+        clean_dogs = self.sanitize(dogs)
+        if len(dogs) != len(clean_dogs):
+            return False
+        queries = []
+        for dog in clean_dogs:
+            sql = "INSERT INTO dogs(name) VALUES(%s)"
+            queries.append({"sql": sql, "bind": dog['name']})
+        db = Db.get_instance()
+        result = db.transactional(queries)
+        return dogs
 
-    def put(self, dog_id=None):
-        if dog_id is not None:
-            dog = self.check(dog_id)
-            if not isinstance(dog, dict):
+    def read(self, filters=None, count_only=False):
+        db = Db.get_instance()
+        fields = ['*']
+        offset = 0
+        limit = 5
+        if filters is not None:
+            if 'fields' in filters:
+                tmp_fields = []
+                for field in filters['fields']:
+                    if field in ['id', 'name']:
+                        tmp_fields.append(field)
+                if len(tmp_fields) > 0:
+                    fields = tmp_fields
+            if 'id' in filters:
+                sql = "SELECT " + ','.join(fields) + " FROM dogs WHERE id = %s"
+                dog = db.fetchone(sql, filters['id'])
                 return dog
-            dog_data = request.json
-            dog_data['id'] = dog_id
-            resp = self._instance.update(dog_data)
-            if resp == False:
-                return make_response(jsonify({
-                    "error": "Failed to update. There are items in your request that are invalid."
-                }), 400)
-            return jsonify(resp)
-        return jsonify(self._instance.update(request.json))
+            if 'offset' in filters:
+                offset = int(filters['offset'])
+            if 'limit' in filters:
+                limit = int(filters['limit'])
+        cols = 'COUNT(*) AS total' if count_only else ','.join(fields)
+        sql = "SELECT " + cols + " FROM dogs"
+        if not count_only:
+            sql += " ORDER BY name LIMIT " + str(offset) + ", " + str(limit)
+        if count_only:
+            row = db.fetchone(sql)
+            return row['total'] if row else 0
+        else:
+            return db.fetchall(sql)
 
-    def delete(self, dog_id=None):
-        if dog_id is not None:
-            dog = self.check(dog_id)
-            if not isinstance(dog, dict):
-                return dog
-            return jsonify(self._instance.delete(dog_id))
-        return jsonify(self._instance.delete(request.json))
+    def update(self, dogs):
+        if not isinstance(dogs, (list, tuple)):
+            dogs = [dogs]
+        clean_dogs = self.sanitize(dogs)
+        if len(dogs) != len(clean_dogs):
+            return False
+        queries = []
+        for dog in clean_dogs:
+            sql = "UPDATE dogs SET name = %s WHERE id = %s"
+            queries.append({"sql": sql, "bind": (dog['name'], dog['id'])})
+        db = Db.get_instance()
+        db.transactional(queries)
+        return dogs
+
+    def delete(self, dogs):
+        counter = 0
+        if not isinstance(dogs, (list, tuple)):
+            dogs = [dogs]
+        placeholder = []
+        queries = []
+        for dog in dogs:
+            placeholder.append('%s')
+        sql = "DELETE FROM dogs WHERE id IN (" + ", ".join(placeholder) + ")"
+        queries.append({"sql": sql, "bind": dogs})
+        db = Db.get_instance()
+        counter = db.transactional(queries)
+        return counter
